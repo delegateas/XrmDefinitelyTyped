7f1707bf14ee6f1beed13b7ea72f59ade38bc1d0
Magnus Gether Sørensen <magesoe@gmail.com>
2018-08-02 12:49:10 +0200

    Refactored build script

--- build.fsx
+++ build.fsx
@@ -10,11 +10,11 @@ open Fake.AssemblyInfoFile
 open Fake.ReleaseNotesHelper
 open System
 open System.IO
-#if MONO
-#else
-#load "packages/SourceLink.Fake/tools/Fake.fsx"
-open SourceLink
-#endif
+//#if MONO
+//#else
+//#load "packages/SourceLink.Fake/tools/Fake.fsx"
+//open SourceLink
+//#endif
 
 
 // --------------------------------------------------------------------------------------
@@ -127,10 +127,6 @@ Target "Clean" (fun _ ->
   CleanDirs ["bin"; "temp"]
 )
 
-Target "CleanDocs" (fun _ ->
-  CleanDirs ["docs/output"]
-)
-
 // --------------------------------------------------------------------------------------
 // Build library & test project
 
@@ -241,151 +237,19 @@ Target "PublishNuget" (fun _ ->
 
 // --------------------------------------------------------------------------------------
 // Generate the documentation
-
-Target "GenerateReferenceDocs" (fun _ ->
-  if not <| executeFSIWithArgs "docs/tools" "generate.fsx" ["--define:RELEASE"; "--define:REFERENCE"] [] then
-    failwith "generating reference documentation failed"
-)
-
-let generateHelp' fail debug =
-  let args =
-    if debug then ["--define:HELP"]
-    else ["--define:RELEASE"; "--define:HELP"]
-  if executeFSIWithArgs "docs/tools" "generate.fsx" args [] then
-    traceImportant (sprintf "Help generated with args: %A" args)
-  else
-    if fail then
-      failwith "generating help documentation failed"
-    else
-      traceImportant "generating help documentation failed"
-
-let generateHelp fail =
-  generateHelp' fail false
-
-let copySpecialFiles () =
-  DeleteFile "docs/content/release-notes.md"
-  CopyFile "docs/content/" "RELEASE_NOTES.md"
-  Rename "docs/content/release-notes.md" "docs/content/RELEASE_NOTES.md"
-
-  DeleteFile "docs/content/license.md"
-  CopyFile "docs/content/" "LICENSE.md"
-  Rename "docs/content/license.md" "docs/content/LICENSE.md"
-
-let copyExtraFiles () =
-  let path = "docs/output/files"
-  CleanDir path
-  // Copy additional files
-  CopyRecursive "files" path true |> tracefn "%A"
-  // Copy over compiled javascript resources
-  CopyRecursive "bin/XrmDefinitelyTyped/Resources" path true |> tracefn "%A"
-
-
-Target "GenerateHelp" (fun _ ->
-  copySpecialFiles()
-  generateHelp true
-  copyExtraFiles()
-)
-
-Target "GenerateHelpDebug" (fun _ ->
-  copySpecialFiles()
-  generateHelp' true true
-  copyExtraFiles()
-)
-
-Target "KeepRunning" (fun _ ->    
-  use watcher = new FileSystemWatcher(DirectoryInfo("docs/content").FullName,"*.*")
-  watcher.EnableRaisingEvents <- true
-  watcher.Changed.Add(fun e -> generateHelp' false true)
-  watcher.Created.Add(fun e -> generateHelp' false true)
-  watcher.Renamed.Add(fun e -> generateHelp' false true)
-  watcher.Deleted.Add(fun e -> generateHelp' false true)
-
-  traceImportant "Waiting for help edits. Press any key to stop."
-
-  System.Console.ReadKey() |> ignore
-
-  watcher.EnableRaisingEvents <- false
-  watcher.Dispose()
-)
-
+Target "CleanDocs" DoNothing
+Target "GenerateReferenceDocs" DoNothing
+Target "GenerateHelp" DoNothing
+Target "GenerateHelpDebug" DoNothing
+Target "KeepRunning" DoNothing
 Target "GenerateDocs" DoNothing
-
-let createIndexFsx lang =
-  let content = """(*** hide ***)
-// This block of code is omitted in the generated HTML documentation. Use 
-// it to define helpers that you do not want to show in the documentation.
-#I "../../../bin"
-
-(**
-F# Project Scaffold ({0})
-=========================
-*)
-"""
-  let targetDir = "docs/content" @@ lang
-  let targetFile = targetDir @@ "index.fsx"
-  ensureDirectory targetDir
-  System.IO.File.WriteAllText(targetFile, System.String.Format(content, lang))
-
-Target "AddLangDocs" (fun _ ->
-  let args = System.Environment.GetCommandLineArgs()
-  if args.Length < 4 then
-    failwith "Language not specified."
-
-  args.[3..]
-  |> Seq.iter (fun lang ->
-    if lang.Length <> 2 && lang.Length <> 3 then
-      failwithf "Language must be 2 or 3 characters (ex. 'de', 'fr', 'ja', 'gsw', etc.): %s" lang
-
-    let templateFileName = "template.cshtml"
-    let templateDir = "docs/tools/templates"
-    let langTemplateDir = templateDir @@ lang
-    let langTemplateFileName = langTemplateDir @@ templateFileName
-
-    if System.IO.File.Exists(langTemplateFileName) then
-      failwithf "Documents for specified language '%s' have already been added." lang
-
-    ensureDirectory langTemplateDir
-    Copy langTemplateDir [ templateDir @@ templateFileName ]
-
-    createIndexFsx lang)
-)
+Target "AddLangDocs" DoNothing
 
 // --------------------------------------------------------------------------------------
 // Release Scripts
 
-Target "ReleaseDocs" (fun _ ->
-  let tempDocsDir = "temp/docs"
-  let tempDocsSubDir = tempDocsDir @@ fullProjectName
-  CleanDir tempDocsDir
-  Repository.cloneSingleBranch "" (docsGitHome + "/" + docsGitName + ".git") 
-    "master" tempDocsDir
-  fullclean tempDocsSubDir
-  CopyRecursive "docs/output" tempDocsSubDir true |> tracefn "%A"
-  StageAll tempDocsSubDir
-  Commit tempDocsSubDir 
-    (sprintf "Update generated documentation for version %s" 
-      release.NugetVersion)
-  Branches.push tempDocsDir)
-
-#load "paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx"
-open Octokit
-
-Target "Release" (fun _ ->
-  StageAll ""
-  Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
-  Branches.push ""
-
-  Branches.tag "" release.NugetVersion
-  Branches.pushTag "" "origin" release.NugetVersion
-    
-  // release on github
-  createClient (getBuildParamOrDefault "github-user" "") (getBuildParamOrDefault "github-pw" "")
-  |> createDraft gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes 
-  // TODO: |> uploadFile "PATH_TO_FILE"    
-  |> releaseDraft
-  |> Async.RunSynchronously
-)
-
+Target "ReleaseDocs" DoNothing
+Target "Release" DoNothing
 Target "BuildPackage" DoNothing
 
 // --------------------------------------------------------------------------------------
@@ -397,37 +261,13 @@ Target "All" DoNothing
   ==> "AssemblyInfo"
   ==> "Build"
   ==> "CopyBinaries"
-//  =?> ("GenerateReferenceDocs",isLocalBuild)
-  ==> "Test"
-  ==> "All"
-
-"BuildSetup"
-  ==> "Build" 
   ==> "Test"
   ==> "NuGet"
   ==> "BuildPackage"
-  ==> "PublishNuget"
-  ==> "Release"
-
+  ==> "All"
   
-"GenerateHelp"
-  //  ==> "GenerateReferenceDocs"
-  ==> "GenerateDocs"
-
-
-[ "GenerateHelp"; "GenerateHelpDebug" ]
-|> List.iter 
-  (fun genHelp ->
-    genHelp <== [ "CopyBinaries"; "CleanDocs" ] |> ignore
-  )
-
-"GenerateHelpDebug"
-  ==> "KeepRunning"
-
-
-"GenerateDocs"
-  ==> "ReleaseDocs"
-  ==> "Release"
+"BuildPackage"
+  ==> "PublishNuget"
 
 
 RunTargetOrDefault "All"
