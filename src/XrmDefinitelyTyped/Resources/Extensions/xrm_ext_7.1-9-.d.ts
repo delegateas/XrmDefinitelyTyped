6d48e43c121437f4657c6c742781373e0d79153e
Thomas Kjærgaard Malowanczyk <tkm@delegate.dk>
2020-06-06 22:30:34 +0200

    Split the different implementation of openQuickCreate. Old implementation is in version 7.1 up to, but not inlcuding 8.2. New implementation is in version 8.2 up to 9 where it becomes depricated.

--- src/XrmDefinitelyTyped/Resources/Extensions/xrm_ext_7.1-9-.d.ts
+++ src/XrmDefinitelyTyped/Resources/Extensions/xrm_ext_7.1-9-.d.ts
@@ -6,22 +6,4 @@ declare namespace Xrm {
      */
     getTimeZoneOffsetMinutes(): number;
   }
-
-  interface OpenQuickCreateResult {
-    /**
-     * Identifies the record displayed or created
-     */
-    savedEntityReference: Lookup;
-}
-
-  interface Utility {
-    /**
-     * Opens a quick create form.
-     *
-     * @param entityLogicalName The logical name of the entity to create.
-     * @param createFromEntity Designates a record that will provide default values based on mapped attribute values.
-     * @param parameters A dictionary object that passes extra query string parameters to the form. Invalid query string parameters will cause an error.
-     */
-    openQuickCreate(entityLogicalName: string, createFromEntity?: Lookup, parameters?: any): Promise<OpenQuickCreateResult>;
-  }
 }
