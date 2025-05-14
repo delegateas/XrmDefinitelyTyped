b5d1beedc66a10eaabca8c450c3c2b201d598b0d
Byron Matus <byron.matus@eon.com>
2025-05-14 10:08:55 +0200

    Add getPageContext method and PageContext interface to Xrm namespace #307

--- src/XrmDefinitelyTyped/Resources/Extensions/xrm_ext_9-.d.ts
+++ src/XrmDefinitelyTyped/Resources/Extensions/xrm_ext_9-.d.ts
@@ -854,6 +854,11 @@ declare namespace Xrm {
         getAllowedStatusTransitions(entityName: string, stateCode: number): Promise<number[] | null>;
 
         /**
+         * returns an object with the input property. The input property is an object with different values depending on whether you are currently on the entity form or entity list.
+         */
+        getPageContext(): PageContext
+
+        /**
          * Returns the entity metadata for the specified entity
          * @param entityName The logical name of the entity.
          * @param attributes The attributes to get metadata for.
@@ -1271,6 +1276,42 @@ declare namespace Xrm {
          * Returns a boolean value indicating if the model-driven apps instance is hosted on-premises or online.
          */
         isOnPremises(): boolean;
+    }
+
+    /**
+     * Interface for the result of calling Utility.getPageContext
+     */
+    interface PageContext {
+        input: {
+            /**
+             * The current page type. The value returned is entityrecord or entitylist
+             */
+            pageType: string;
+            /**
+             * Logical name of the table currently displayed.
+             */
+            entityName: string;
+            /**
+             * ID of the table record currently displayed in the form.
+             */
+            entityId?: string;
+            /**
+             * The parent record that provides default values based on mapped column values. The lookup object has the following String properties: entityType, id, and name.
+             */
+            createFromEntity?: Lookup;
+            /**
+             * ID of the currently displayed form.
+             */
+            formId?: string;
+            /**
+             * ID of the view currently displayed.
+             */
+            viewId?: string;
+            /**
+             * Type of the view currently displayed. Possible values are savedquery or userquery.
+             */
+            viewType?: string;
+        }
     }
 
     /**
